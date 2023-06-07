4bf9f26a4d3ab1ecae5258b2c90b1f82194ba394
David Riley <Dril112191@gmail.com>
2023-06-07 17:06:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, createContext } from "react";
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromChildren, Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromChildren, Outlet, useLocation, Navigate } from "react-router-dom";
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -10,6 +10,12 @@ import GamesPlayed from "./components/GamesPlayed"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const AuthContext = createContext();
+
+const NotFound = () => {
+  const location = useLocation();
+  console.log(`404: Page not found - ${location.pathname}`);
+  return <Navigate to="/" />;
+};
 
 export default function App() {
   const [gamesSearched, setGamesSearched] = useState(false);
@@ -26,10 +32,10 @@ export default function App() {
     createRoutesFromChildren(
       <Route path={"/"} element={<Layout />}>
         <Route index element={<Landing />} />
-        <Route path="*" element={<Landing />} />
         <Route path="/search" element={<GetGames />} />
         <Route path="/games-saved" element={<GamesSaved />}/>
         <Route path="/games-played" element={<GamesPlayed />}/>
+        <Route path="*" element={<NotFound />} />
         
       </Route>
     )
