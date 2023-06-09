c5a8d8864923fb57bd53d6ad7a1a9e12d2163251
David Riley <Dril112191@gmail.com>
2023-06-09 13:53:27 -0400

    Update GameCard.jsx

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -26,7 +26,7 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
     useEffect(() => {
       const updatedDisplayedGames = games.slice(startIndex, endIndex);
       setDisplayedGames(updatedDisplayedGames);
-    }, [games, startIndex, endIndex]);
+    }, [games, startIndex, endIndex, currentPage]);
 
     const fetchGames = async () => {
       try {
