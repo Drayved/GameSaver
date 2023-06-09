5b7e0f0f36aded37df1a300eba4b1763838df254
David Riley <Dril112191@gmail.com>
2023-06-09 16:33:00 -0400

    Update GameCard.jsx

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,27 +1,19 @@
 import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../App";
-import { collection, doc, setDoc, deleteDoc, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, setDoc, deleteDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
-export default function GameCard({ currentPage, setCurrentPage, totalPages, setTotalPages, loading, setLoading}){
+export default function GameCard({ currentPage, setCurrentPage, totalPages, setTotalPages}){
     const { games, setGames, user } = useContext(AuthContext)
     const startIndex = (currentPage - 1) * 3;
     let endIndex = startIndex + 3;
     const [displayedGames, setDisplayedGames] = useState(games.slice(startIndex, endIndex))
     const [gamesAdded, setGamesAdded] = useState(true)
-    
 
     const isGamesSavedPage = window.location.pathname === "/games-saved";
     const isPlayedGamesPage = window.location.pathname === "/games-played";
     const isSearchPage = window.location.pathname === "/search"
     const [currentPageType, setCurrentPageType] = useState(isGamesSavedPage ? "saved" : "played")
-
-   
-
-    useEffect(() => {
-      const updatedDisplayedGames = games.slice(startIndex, endIndex);
-      setDisplayedGames(updatedDisplayedGames);
-    }, [currentPage, startIndex, endIndex]);
 
     useEffect(() => {
       setCurrentPageType(isGamesSavedPage ? "saved" : "played");
@@ -30,31 +22,36 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
     useEffect(() => {
       displayedGames <= 0 && setGamesAdded(false)
     }, [displayedGames])
+
+    // displayedGames in below useEffect causes flickering, removing it causes issues with games being transferred from list to list. May need code refactoring.
   
-
-
     useEffect(() => {
-      const fetchGames = async () => {
-        try {
-          let fetchedGames = [];
-          if (isGamesSavedPage) {
-            const querySnapshot = await getDocs(
-              query(collection(db, "wantToPlay"), where("game.played", "==", false))
-            );
-            fetchedGames = querySnapshot.docs.map((doc) => doc.data().game);
-          } else if (isPlayedGamesPage) {
-            const querySnapshot = await getDocs(
-              query(collection(db, "playedGames"), where("game.played", "==", true))
-            );
-            fetchedGames = querySnapshot.docs.map((doc) => doc.data().game);
-          }
-          setGames(fetchedGames);
-        } catch (error) {
-          console.log("Error fetching games:", error);
-        }
-      };
+      const updatedDisplayedGames = games.slice(startIndex, endIndex);
+      setDisplayedGames(updatedDisplayedGames);
+    }, [displayedGames, currentPage, startIndex, endIndex, currentPageType]);
   
+    useEffect(() => {
       if (user) {
+        const fetchGames = async () => {
+          try {
+            let fetchedGames = [];
+            if (isGamesSavedPage) {
+              const querySnapshot = await getDocs(
+                query(collection(db, "wantToPlay"), where("game.played", "==", false))
+              );
+              fetchedGames = querySnapshot.docs.map((doc) => doc.data().game);
+            } else if (isPlayedGamesPage) {
+              const querySnapshot = await getDocs(
+                query(collection(db, "playedGames"), where("game.played", "==", true))
+              );
+              fetchedGames = querySnapshot.docs.map((doc) => doc.data().game);
+            }
+            setGames(fetchedGames);
+          } catch (error) {
+            console.log("Error fetching games:", error);
+          }
+        };
+  
         fetchGames();
       }
     }, [user, isGamesSavedPage, isPlayedGamesPage, setGames]);
@@ -66,19 +63,19 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
           const gameData = { game };
           const collectionName = currentPageType === "played" ? "playedGames" : "wantToPlay";
     
-          
+          // Delete the game from the original list if it exists in Firebase
           await deleteDoc(doc(collection(db, collectionName), game.id.toString()));
     
-         
+          // Add the game to the new list in Firebase
           await setDoc(doc(collection(db, "wantToPlay"), game.id.toString()), gameData);
     
-          
+          // Update the local state accordingly
           const remainingGames = games.filter((g) => g.id !== game.id);
           setGames(remainingGames);
 
           if (remainingGames.length === 0 && currentPage > 1) {
-            setCurrentPage((prevPage) => prevPage - 1); 
-            return; 
+            setCurrentPage((prevPage) => prevPage - 1); // Navigate to previous page
+            return; // Exit the function to prevent further operations
           }
 
           setDisplayedGames((prevGames) => prevGames.filter((g) => g.id !== game.id));
@@ -86,7 +83,7 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
           console.log("Game moved to the 'I want to play' list!");
         } else {
           console.log("User not authenticated.");
-          
+          // You can add a logic to show a message or redirect the user to the sign-in page
         }
       } catch (error) {
         console.log("Error moving game to the 'I want to play' list:", error);
@@ -100,19 +97,19 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
           const gameData = { game };
           const collectionName = currentPageType === "played" ? "playedGames" : "wantToPlay";
     
-         
+          // Delete the game from the original list if it exists in Firebase
           await deleteDoc(doc(collection(db, collectionName), game.id.toString()));
     
-         
+          // Add the game to the new list in Firebase
           await setDoc(doc(collection(db, "playedGames"), game.id.toString()), gameData);
     
-         
+          // Update the local state accordingly
           const remainingGames = games.filter((g) => g.id !== game.id);
           setGames(remainingGames);
 
           if (remainingGames.length === 0 && currentPage > 1) {
-            setCurrentPage((prevPage) => prevPage - 1); 
-            return; 
+            setCurrentPage((prevPage) => prevPage - 1); // Navigate to previous page
+            return; // Exit the function to prevent further operations
           }
 
           setDisplayedGames((prevGames) => prevGames.filter((g) => g.id !== game.id));
@@ -120,7 +117,7 @@ export default function GameCard({ currentPage, setCurrentPage, totalPages, setT
           console.log("Game moved to the 'Played it' list!");
         } else {
           console.log("User not authenticated.");
-          
+          // You can add a logic to show a message or redirect the user to the sign-in page
         }
       } catch (error) {
         console.log("Error moving game to the 'Played it' list:", error);
@@ -132,11 +129,11 @@ const handleDelete = async (game) => {
     if (user) {
       const collectionName = currentPageType === "saved" ? "wantToPlay" : "playedGames";
 
-     
+      // Delete the game from Firebase
       await deleteDoc(doc(collection(db, collectionName), game.id.toString()));
       console.log("Game deleted successfully!");
 
-     
+      // Remove the game from the local state
       const remainingGames = games.filter((g) => g.id !== game.id);
       setGames(remainingGames);
 
@@ -144,11 +141,11 @@ const handleDelete = async (game) => {
       setDisplayedGames(remainingDisplayedGames);
 
       if (remainingDisplayedGames.length === 0 && currentPage > 1) {
-        setCurrentPage((prevPage) => prevPage - 1); 
+        setCurrentPage((prevPage) => prevPage - 1); // Navigate to previous page
       }
     } else {
       console.log("User not authenticated.");
-      
+      // You can add a logic to show a message or redirect the user to the sign-in page
     }
   } catch (error) {
     console.log("Error deleting game:", error);
@@ -170,107 +167,78 @@ const handleDelete = async (game) => {
 
 
 
-    return (
-      <div className="games-card-container">
-        {loading ? (
-          <div className="loading-message"></div>
-        ) : (
-          displayedGames.map((game) => (
-            <div key={game.id} className="games-card">
-              <h3 className="game-name">{game.name}</h3>
-              <div className="game-info">
-                <img
-                  className="game-img"
-                  src={game.background_image}
-                  alt={game.name}
-                ></img>
-                <div className="game-details">
-                  <div className="game-release">
-                    <p>Released: </p>
-                    <p className="release">{game.released}</p>
-                  </div>
-                  <div className="game-genres">
-                    <p>Genres:</p>
-                    <p className="genres">
-                      {" "}
-                      {game.genres.map((genre) => genre.name).slice(0, 2).join(", ")}
-                    </p>
-                  </div>
+    return(
+        <div className="games-card-container">
+        {displayedGames.map((game) => (
+          <div key={game.id} className="games-card">
+            <h3 className="game-name">{game.name}</h3>
+            <div className="game-info">
+              <img
+                className="game-img"
+                src={game.background_image}
+                alt={game.name}
+              ></img>
+              <div className="game-details">
+                <div className="game-release">
+                  <p>Released: </p>
+                  <p className="release">{game.released}</p>
                 </div>
+                <div className="game-genres">
+                  <p>Genres:</p>
+                  <p className="genres"> {game.genres.map((genre) => genre.name).slice(0, 2).join(", ")}</p>
+                </div>
+                
               </div>
-              <div className="more-info-container">
-                <button
-                  onClick={() =>
-                    window.open(
-                      `https://rawg.io/games/${game.name
-                        .replace(/\s/g, "-")
-                        .replace(/:/g, "")}`
-                    )
-                  }
-                  className="more-info-btn"
-                >
-                  More Info
-                </button>
-              </div>
-              <div className="list-btns">
-                {!isGamesSavedPage && (
-                  <button
-                    onClick={(event) => handleWantToPlay(event, game)}
-                    className="want-btn"
-                  >
-                    I want to play it
-                  </button>
-                )}
-                {!isPlayedGamesPage && (
-                  <button
-                    onClick={(event) => handlePlayedIt(event, game)}
-                    className="played-btn"
-                  >
-                    I played it
-                  </button>
-                )}
-    
-                {isSearchPage ? (
-                  ""
-                ) : (
-                  <button
-                    className="remove-btn"
-                    onClick={() => handleDelete(game)}
-                  >
-                    Remove
-                  </button>
-                )}
-              </div>
+              
             </div>
-          ))
-        )}
-    
-        <div
-          className={`${!gamesAdded && window.location.pathname !== "/search" ? "hidden" : ""}`}
-        >
-          <div className="page-container page-bottom">
-            <button
-              className="prev-page font-semibold"
-              onClick={handlePreviousPage}
-              disabled={currentPage === 1}
-            >
-              Previous Page
-            </button>
-            <p className="page-dash">-</p>
-            <button
-              className="next-page font-semibold"
-              onClick={handleNextPage}
-              disabled={currentPage === totalPages}
-            >
-              Next Page
-            </button>
+            <div className="more-info-container">
+              <button onClick={() => window.open(`https://rawg.io/games/${game.name.replace(/\s/g, "-").replace(/:/g, "")}`)} className="more-info-btn">More Info</button>
+            </div>
+            
+            <div className="list-btns">
+            {!isGamesSavedPage && (
+              <button onClick={(event) => handleWantToPlay(event, game)} className="want-btn">
+                I want to play it
+              </button>
+            )}
+            {!isPlayedGamesPage && (
+              <button onClick={(event) => handlePlayedIt(event, game)} className="played-btn">
+                I played it
+              </button>
+            )}
+          
+            {isSearchPage ? "" : 
+                <button className="remove-btn" onClick={() => handleDelete(game)}>
+                  Remove
+                </button>
+            }  
+            </div>
           </div>
-          <p className={`page-displayed hidden`}>
-            {currentPage} - {totalPages}
-          </p>
+        ))}
+        
+        
+        <div className={`${!gamesAdded && window.location.pathname !== "/search" ? "hidden" : ""}`}>
+        <div className="page-container page-bottom">
+          <button
+            className="prev-page font-semibold"
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+          >
+            Previous Page
+          </button>
+          <p className="page-dash">-</p>
+          <button
+            className="next-page font-semibold"
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next Page
+          </button>
         </div>
+        <p className={`page-displayed`}>{currentPage} - {totalPages}</p>
+        </div>
+  
+        
       </div>
-    );
-    
-    
+    )
 }
