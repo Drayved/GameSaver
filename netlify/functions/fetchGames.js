f1a81787beae12008c6bcc3ad6b3e21ba94b653a
David Riley <Dril112191@gmail.com>
2023-06-29 16:34:04 -0400

    Update fetchGames.js

--- netlify/functions/fetchGames.js
+++ netlify/functions/fetchGames.js
@@ -1,39 +1,10 @@
 
 
 export default async function handler(event){
-  try {
-    const { search, genres } = event.queryStringParameters;
-    const apiKey = "10cab07048cb4f6591685d4bf79954bd";
-
-    let apiUrl = `https://api.rawg.io/api/games?key=${apiKey}`;
-
-    if (search) {
-      apiUrl += `&search=${search}`;
-    }
-
-    if (genres) {
-      apiUrl += `&genres=${genres}`;
-    }
-
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: JSON.stringify({ error: 'Something went wrong' }),
-    };
-  }
+  return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'This is what will be returned!'
+		})
+	}
 }
