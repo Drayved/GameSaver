15a244a46e6a450b27a78b5b03f57a95068c4475
David Riley <Dril112191@gmail.com>
2023-06-29 16:10:51 -0400

    Update fetchGames.js

--- netlify/functions/fetchGames.js
+++ netlify/functions/fetchGames.js
@@ -18,10 +18,7 @@ export const handler = async (event) => {
 
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
+      
       body: JSON.stringify(data),
     };
   } catch (error) {
