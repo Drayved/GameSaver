9cb8ee76e4bb5da4930f7ac56b40a532b23f12d2
David Riley <Dril112191@gmail.com>
2023-06-30 12:25:03 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, createContext } from "react"
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromChildren, Outlet, useLocation, Navigate } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromChildren, Outlet, useLocation, Navigate, useNavigate } from "react-router-dom"
 import Landing from "./components/Landing"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
@@ -15,7 +15,14 @@ export const AuthContext = createContext()
 const NotFound = () => {
   const location = useLocation()
   console.log(`404: Page not found - ${location.pathname}`)
-  return <Navigate to="/" />
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const navigateToLanding = () => navigate("/", { replace: true })
+    navigateToLanding()
+  }, [])
+
+  return null
 }
 
 export default function App() {
@@ -39,11 +46,11 @@ export default function App() {
   const router = createBrowserRouter(
     createRoutesFromChildren(
       <Route path={"/"} element={<Layout />}>
-        <Route index element={<Landing />} />
+        <Route path={"/"} element={<Landing />} />
         <Route path="/search" element={<GetGames />} />
         <Route path="/games-saved" element={<GamesSaved />}/>
         <Route path="/games-played" element={<GamesPlayed />}/>
-        <Route path="*" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
         
       </Route>
     )
